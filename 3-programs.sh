f093f4815fac99f01b3d2f4cbd407cabf2248daf
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-05-29 15:06:12 +0000

    Create 3-programs.sh

---
+++ 3-programs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo
+echo "      _        _                                     ___                               "
+echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
+echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/ _' |/ _' |  "
+echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
+echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
+echo "                                                                         |___/         "
+echo "                        Archlinux Post-Install Setup and Config"
+echo
+
+if ! [ $EUID -ne 0 ]; then
+    echo "Don't run this program as root."
+    exit 1
+fi
+
+echo
+echo "Installing Paru"
+echo
+cd ~
+git clone https://aur.archlinux.org/paru.git && cd paru
+makepkg -si
+sleep 1s
+cd ~
+rm -rf paru
