a172f5ba0700fc15b19c9425ced47fa6369ee795
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-05-30 10:32:05 +0000

    Update 3-programs.sh

--- Major/DailyDriver/3-programs.sh
+++ Major/DailyDriver/3-programs.sh
@@ -16,3 +16,77 @@ if ! [ $EUID -ne 0 ]; then
     echo
     exit 1
 fi
+
+PKGX=(
+    ''                                              # Description
+    ''                                              # Description
+)
+
+for PKG in "${PKGX[@]}"; do
+    echo
+    echo "INSTALLING: ${PKG}"
+    echo
+    sudo pacman -S "$PKG" --noconfirm --needed
+    echo
+    sleep 1s
+done
+
+PKGY=(
+    ''                                              # Description
+    ''                                              # Description
+)
+
+for PKG in "${PKGY[@]}"; do
+    echo
+    echo "INSTALLING: ${PKG}"
+    echo
+    paru -S "$PKG" --noconfirm --needed
+    echo
+    sleep 1s
+done
+
+PKGZ=(
+    'org.mozilla.firefox'                           # Firefox developer edition
+    'org.onlyoffice.desktopeditors'                 # Open-source office suite ("replaces" MS Word, PowerPoint and Excel)
+    'org.kde.okteta'                                # Hex Editor
+    'org.kde.kleopatra'                             # Certificate Manager and Unified Crypto GUI
+    'org.qbittorrent.qBittorrent'                   # Torrent app
+    'io.mpv.Mpv'                                    # Media player
+    'org.gimp.GIMP'                                 # GNU Image Manipulator
+    'org.kde.krita'                                 # Digital Painting Software
+    'org.inkscape.Inkscape'                         # Vector Graphics Editor
+    'org.blender.Blender'                           # 3D Modelling Software
+    'fr.handbrake.ghb'                              # Transcoder
+    'io.github.Qalculate.qalculate-qt'              # Calculator
+    'com.spotify.Client'                            # Spotify
+    'com.obsproject.Studio'                         # Streaming software
+    'io.github.achetagames.epic_asset_manager'      # Epic Games' Marketplace for Linux
+    ''
+)
+
+for PKG in "${PKGZ[@]}"; do
+    echo
+    echo "INSTALLING: ${PKG}"
+    echo
+    flatpak install flathub "$PKG" -y --or-update
+    echo
+    sleep 1s
+done
+
+sleep 1s
+
+echo
+echo " Done!"
+echo
+
+sleep 1s
+
+echo
+echo "press Y to reboot now or N if you plan to manually reboot later."
+echo
+
+read REBOOT
+if [ ${REBOOT,,} = y ]; then
+    sudo reboot now
+fi
+exit 0
