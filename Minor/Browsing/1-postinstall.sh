4ebb8e5f674ef5bbea7d3c0e411f5f5564b19e1e
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-05-30 10:37:26 +0000

    Create 1-postinstall.sh

---
+++ Minor/Browsing/1-postinstall.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+echo
+echo "      _        _                                     ___                               "
+echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
+echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/ _' |/ _' |  "
+echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
+echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
+echo "                                                                         |___/         "
+echo "                        Archlinux Post-Install Setup and Config"
+echo
+
+echo
+echo "INSTALLING POST-INSTALL STUFF"
+echo
+
+if ! [ $EUID -ne 0 ]; then
+    echo
+    echo "Don't run this script as root."
+    echo
+    sleep 1s
+    exit 1
+fi
+
+sleep 1s
+
+sudo pacman -Syu --noconfirm --needed
+sudo pacman -S flatpak --noconfirm --needed
+
+sleep 1s
+
+echo
+echo "Press Y to reboot now or N if you plan to manually reboot later."
+echo
+
+read REBOOT
+if [ ${REBOOT,,} = y ]; then
+    reboot
+fi
+exit 0
