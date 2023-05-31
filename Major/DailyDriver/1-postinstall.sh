b1b94084f42e110b4879533a35abbeec8bfa607e
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-05-31 14:14:40 +0000

    Update 1-postinstall.sh

--- Major/DailyDriver/1-postinstall.sh
+++ Major/DailyDriver/1-postinstall.sh
@@ -73,7 +73,7 @@ sleep 1s
 echo
 echo "Enabling Color and ILoveCandy"
 echo
-sudo sed -i "s|#Color.*|Color\nILoveCandy|g" /etc/pacman.conf
+sudo sed "s|#Color.*|Color\nILoveCandy|g" /etc/pacman.conf
 
 sleep 1s
 
@@ -90,7 +90,7 @@ curl -o "/home/$(logname)/Downloads/mirrorlist" 'https://archlinux.org/mirrorlis
 echo
 echo "Uncomenting \"#Server\" from mirrorlist"
 echo
-sed -i 's/#S/S/g' "/home/$(logname)/Downloads/mirrorlist"
+sed 's/#S/S/g' "/home/$(logname)/Downloads/mirrorlist"
 echo
 echo "Ranking mirrors, this will take a while"
 echo
@@ -180,13 +180,13 @@ sleep 1s
 echo
 echo "Setting CPU governor to Performance"
 echo
-sudo sed -i "s|#governor=.*|governor='performance'|g" /etc/default/cpupower
+sudo sed "s|#governor=.*|governor='performance'|g" /etc/default/cpupower
 sleep 1s
 echo
 echo "Setting minimum and maximum CPU Frequencies"
 echo
-sudo sed -i "s|#min_freq=.*|min_freq=\"3.7GHz\"|g" /etc/default/cpupower
-sudo sed -i "s|#max_freq=.*|max_freq=\"4.2GHz\"|g" /etc/default/cpupower
+sudo sed "s|#min_freq=.*|min_freq=\"3.7GHz\"|g" /etc/default/cpupower
+sudo sed "s|#max_freq=.*|max_freq=\"4.2GHz\"|g" /etc/default/cpupower
 sleep 1s
 echo
 echo "Enabling cpupower service"
@@ -198,9 +198,9 @@ sleep 1s
 echo
 echo "Set make to be multi-threaded by default"
 echo
-sudo sed -i 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(expr $(nproc) \+ 1)"|g' /etc/makepkg.conf
+sudo sed "s|MAKEFLAGS=.*|MAKEFLAGS=\"-j$(expr $(nproc) \+ 1)\"|g" /etc/makepkg.conf
 sleep 1s
-sudo sed -i "s|COMPRESSXZ=.*|COMPRESSXZ=(xz -c -T $(expr $(nproc) \+ 1) -z -)|g" /etc/makepkg.conf
+sudo sed "s|COMPRESSXZ=.*|COMPRESSXZ=(xz -c -T $(expr $(nproc) \+ 1) -z -)|g" /etc/makepkg.conf
 
 sleep 1s
 
@@ -259,8 +259,8 @@ sleep 1s
 echo
 echo "Setting vifm as paru's File Manager"
 echo
-sudo sed -i 's/#[bin]/[bin]/g' "/etc/paru.conf"
-sudo sed -i 's/#FileManager/FileManager/g' "/etc/paru.conf"
+sudo sed 's/#[bin]/[bin]/g' /etc/paru.conf
+sudo sed 's/#FileManager/FileManager/g' /etc/paru.conf
 
 sleep 1s
 
@@ -278,9 +278,9 @@ fi
 sleep 1s
 
 if grep -q -F "DefaultProfile=" "/home/$(logname)/.config/konsolerc"; then
-    sed -i "s|DefaultProfile=.*|DefaultProfile=$(logname).profile|g" "/home/$(logname)/.config/konsolerc"
+    sed "s|DefaultProfile=.*|DefaultProfile=$(logname).profile|g" "/home/$(logname)/.config/konsolerc"
 elif ! grep -q -F "DefaultProfile=" "/home/$(logname)/.config/konsolerc" && ! grep -q -F "[Desktop Entry]" "/home/$(logname)/.config/konsolerc"; then
-    sed -i "1 i\[Desktop Entry]\nDefaultProfile=$(logname).profile\n" "/home/$(logname)/.config/konsolerc"
+    sed "1 i\[Desktop Entry]\nDefaultProfile=$(logname).profile\n" "/home/$(logname)/.config/konsolerc"
 fi
 
 sleep 1s
@@ -299,9 +299,9 @@ if pacman -Q | grep 'yakuake'; then
     sleep 1s
 
     if grep -q -F "DefaultProfile=" "/home/$(logname)/.config/yakuakerc"; then
-        sed -i "s|DefaultProfile=.*|DefaultProfile=$(logname).profile|g" "/home/$(logname)/.config/yakuakerc"
+        sed "s|DefaultProfile=.*|DefaultProfile=$(logname).profile|g" "/home/$(logname)/.config/yakuakerc"
     elif ! grep -q -F "DefaultProfile=" "/home/$(logname)/.config/yakuakerc" && ! grep -q -F "[Desktop Entry]" "/home/$(logname)/.config/yakuakerc"; then
-        sed -i "1 i\[Desktop Entry]\nDefaultProfile=$(logname).profile\n" "/home/$(logname)/.config/yakuakerc"
+        sed "1 i\[Desktop Entry]\nDefaultProfile=$(logname).profile\n" "/home/$(logname)/.config/yakuakerc"
     fi
     
     #sleep 1s
