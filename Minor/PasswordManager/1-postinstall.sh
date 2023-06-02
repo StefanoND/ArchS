485eeafaaabfbae0fd0750a1c7165671bc951b4c
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-06-02 09:29:31 +0000

    Update 1-postinstall.sh

--- Minor/PasswordManager/1-postinstall.sh
+++ Minor/PasswordManager/1-postinstall.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-echo
-echo "      _        _                                     ___                               "
-echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
-echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/ _' |/ _' |  "
-echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
-echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
-echo "                                                                         |___/         "
-echo "                        Archlinux Post-Install Setup and Config"
-echo
-
-echo
-echo "INSTALLING POST-INSTALL STUFF"
-echo
-
 if ! [ $EUID -ne 0 ]; then
     echo
     echo "Don't run this script as root."
@@ -22,18 +8,44 @@ if ! [ $EUID -ne 0 ]; then
     exit 1
 fi
 
+clear
+echo
+echo
+echo "      _        _                                     ___                               "
+echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
+echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/ _' |/ _' |  "
+echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
+echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
+echo "                                                                         |___/         "
+echo "                                  Post-Install Script"
+echo
+echo
+sleep 2s
+clear
+
+sleep 1s
+echo
+echo "Updating system"
+echo
 sleep 1s
 sudo pacman -Syu --noconfirm --needed
+sleep 1s
+echo
+echo "Installing Flatpak"
+echo
 sleep 1s
 sudo pacman -S flatpak --noconfirm --needed
 sleep 1s
 
 echo
+echo "Done."
+echo
+sleep 1s
+echo
 echo "Press Y to reboot now or N if you plan to manually reboot later."
 echo
-
 read REBOOT
-if [ ${REBOOT,,} = y ]; then
+if [[ ${REBOOT,,} = y ]]; then
     reboot
 fi
 exit 0
