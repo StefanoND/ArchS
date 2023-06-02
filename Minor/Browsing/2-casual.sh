bcb97f7f25236f82f3954c60617602c282f4a930
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-06-02 09:46:05 +0000

    Update 2-casual.sh

--- Minor/Browsing/2-casual.sh
+++ Minor/Browsing/2-casual.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+if ! [ $EUID -ne 0 ]; then
+    echo
+    echo "Don't run this script as root."
+    echo
+    sleep 1s
+    exit 1
+fi
+
+clear
+echo
 echo
 echo "      _        _                                     ___                               "
 echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
@@ -7,21 +17,11 @@ echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/
 echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
 echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
 echo "                                                                         |___/         "
-echo "                        Archlinux Post-Install Setup and Config"
+echo "                                  Post-Install Script"
 echo
-
-if ! [ $EUID -ne 0 ]; then
-    echo
-    echo "Don't run this script as root."
-    echo
-    exit 1
-fi
-
 echo
-echo "INSTALLING PROGRAMS"
-echo
-
-sleep 1s
+sleep 2s
+clear
 
 PKGS=(
     'org.mozilla.firefox'                           # Firefox
@@ -38,14 +38,7 @@ for PKG in "${PKGS[@]}"; do
     sleep 1s
 done
 
-sleep 1s
-
 echo
-echo "Press Y to reboot now or N if you plan to manually reboot later."
+echo "Done."
 echo
-
-read REBOOT
-if [ ${REBOOT,,} = y ]; then
-    reboot
-fi
 exit 0
