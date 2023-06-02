f0fcf244c3e14b8aca462dc40bbd2f9d36604ac6
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-06-02 14:12:15 +0000

    Create 3-bridge.sh

---
+++ Hypervisor/3-bridge.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if ! [ $EUID -ne 0 ]; then
+    echo
+    echo "Don't run this script as root."
+    echo
+    sleep 1s
+    exit 1
+fi
+
+clear
+echo
+echo
+echo "      _        _                                     ___                               "
+echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
+echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/ _' |/ _' |  "
+echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
+echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
+echo "                                                                         |___/         "
+echo "                                  Post-Install Script"
+echo
+echo
+sleep 2s
+clear
+
+# TODO
+
+sleep 1s
+echo
+echo "Done..."
+echo
+sleep 1s
+echo
+echo "Press Y to reboot now or N if you plan to manually reboot later."
+echo
+read REBOOT
+if [ ${REBOOT,,} = y ]; then
+    reboot
+fi
+exit 0
