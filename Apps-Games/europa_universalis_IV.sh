850187b500b796e6953ba0a6c599e996a7f66b4b
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-06-04 15:05:46 +0000

    Create europa_universalis_IV.sh

---
+++ Apps-Games/europa_universalis_IV.sh
@@ -0,0 +1,14 @@
+# This is not a runnable script
+#
+# Launcher: Heroic Games Launcher
+#
+# Install without changing anything.
+#
+# After installing run the game once, it'll open up the  Paradox Interacive launcher, launch it, log in, run the game, the game will crash,
+# Close the launcher and edit the game's settings
+#
+# Change "Wine Version" to "Wine - lutris-X.X-X-x86_64" (Where the X are the version number)
+# Click on "Other" and "Select an alternative EXE to run" and choose "eu4.exe"
+# Optional: Check "Enable Esync", "Enable Fsync", "Enable FSR Hack" (I chose strength of 2), "Use GameMode" and "Enable Mangohud"
+#
+# For the FSR to work you must change the game's resolution to a resolution lower than your native resolution.
