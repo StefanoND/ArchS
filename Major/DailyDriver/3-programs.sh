6aac1200d04ccec9b15231b368fb682ecb18a7a9
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-06-04 07:56:20 +0000

    Update 3-programs.sh

--- Major/DailyDriver/3-programs.sh
+++ Major/DailyDriver/3-programs.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+if ! [ $EUID -ne 0 ]; then
+    echo
+    echo "Don't run this script as root."
+    echo
+    sleep 1s
+    exit 1
+fi
+
+clear
+echo
 echo
 echo "      _        _                                     ___                               "
 echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
@@ -7,15 +17,13 @@ echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/
 echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
 echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
 echo "                                                                         |___/         "
-echo "                        Archlinux Post-Install Setup and Config"
+echo "                                  Post-Install Script"
 echo
+echo
+sleep 2s
+clear
 
-if ! [ $EUID -ne 0 ]; then
-    echo
-    echo "Don't run this script as root."
-    echo
-    exit 1
-fi
+sleep 1s
 
 # PACMAN
 PKGX=(
