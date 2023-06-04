c83342b2fc26409bb2bc76e576eb78f5531dc7ae
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-06-04 14:20:26 +0000

    Create spotify_auto_close_after_disabling_cookies.sh

---
+++ Troubleshooting/spotify_auto_close_after_disabling_cookies.sh
@@ -0,0 +1,16 @@
+# This is not a runnable script
+#
+#After disabling cookies in spotify is would automatically close
+#Sometimes it would complain about not finding a GPU
+#Sometimes it would complain about missing libs
+#Going into its prefs
+nano ~/.config/spotify/prefs
+#
+# And change this line from true
+"app.cookies-disabled=true"
+#
+# to false
+"app.cookies-disabled=false"
+#
+# Or just run the command below
+sed -i "s|app.cookies-disabled=.*|app.cookies-disabled=false|g" /home/$(logname)/.config/spotify/prefs
