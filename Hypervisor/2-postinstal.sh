61965dd37cde7b5208e01f7278166bbd2136f765
Stefano Dobbin <45202623+StefanoND@users.noreply.github.com>
2023-06-05 09:06:15 +0000

    Create 2-postinstal.sh

---
+++ Hypervisor/2-postinstal.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+if ! [ $EUID -ne 0 ]; then
+    echo
+    echo "Don't run this script as root."
+    echo
+    sleep 1s
+    exit 1
+fi
+
+clear
+echo
+echo
+echo "      _        _                                     ___                               "
+echo "     / \   ___| |_ ___ _ __ _ __  _   _ _ __ ___    / _ \ _ __ ___   ___  __ _  __ _   "
+echo "    / _ \ / _ \ __/ _ \ '__| '_ \| | | | '_ ' _ \  | | | | '_ ' _ \ / _ \/ _' |/ _' |  "
+echo "   / ___ \  __/ ||  __/ |  | | | | |_| | | | | | | | |_| | | | | | |  __/ (_| | (_| |  "
+echo "  /_/   \_\___|\__\___|_|  |_| |_|\__,_|_| |_| |_|  \___/|_| |_| |_|\___|\__, |\__,_|  "
+echo "                                                                         |___/         "
+echo "                                  Post-Install Script"
+echo
+echo
+sleep 2s
+clear
+
+sleep 1s
+
+# FLATPAK
+PKGZ=(
+    'com.github.tchx84.Flatseal'                    # Flatpak permission manager
+    'org.mozilla.firefox'                           # Firefox Browser
+)
+
+for PKG in "${PKGZ[@]}"; do
+    echo
+    echo "INSTALLING: ${PKG}"
+    echo
+    flatpak install flathub "$PKG" -y --or-update
+    echo
+    sleep 1s
+done
+
+sleep 1s
+
+echo
+echo "Done"
+echo
+exit 0
